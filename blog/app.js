91bee7dbe90945153d1f3cae2c1a52986f9b3fd7
mikan <lsmxinjing@gmail.com>
2014-05-05 17:15:37 +0800

    blog

--- blog/app.js
+++ blog/app.js
@@ -2,5 +2,5 @@ var express = require('express')
   , Resource = require('express-resource')
   , app = express();
 
-app.resource('/', require('./router/forum'));
+app.resource(require('./mvc/controller'));
 app.listen(3000);
